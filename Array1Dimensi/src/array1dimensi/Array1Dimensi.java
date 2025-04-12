26ba658003f6da8fb9d8bf324edff24edd5e1191
OOP C <arthawebid@gmail.com>
2025-04-12 14:27:47 +0800

    Update Project

--- Array1Dimensi/src/array1dimensi/Array1Dimensi.java
+++ Array1Dimensi/src/array1dimensi/Array1Dimensi.java
@@ -1,5 +1,7 @@
 package array1dimensi;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Made Artha
@@ -11,7 +13,37 @@ public class Array1Dimensi {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String[] NMA = new String[5];
+        
+        NMA[0]="Johan";
+        NMA[1]="Wati";
+        NMA[2]="Sukreni";
+        NMA[3]="Warsito";
+        NMA[4]="Sugeng";
+        
+        Scanner inp = new Scanner(System.in);
+        
+        System.out.println("~~~~~Input Data Ke Array~~~~~");
+        for(int idx=0; idx<5; idx++ ){
+            System.out.printf("Data Nama Pengunjung ke-%d: ", idx+1);
+            NMA[idx] = inp.nextLine();
+        }
+        
+        System.out.println("~~~~~Output Data Ke Array~~~~~");
+        for(int idx=0; idx<5; idx++ ){
+  System.out.println("Data Nama Pengunjung ke-"+(idx+1)+": "+NMA[idx]);
+        }
+        
+        System.out.println("~~~~~Output2 Data Ke Array~~~~~");
+        int no=1;
+        //foreach
+        for(String isi : NMA){
+            System.out.println(no+". "+isi);
+            no++;
+        }
+        
+        
+        
     }
     
 }
