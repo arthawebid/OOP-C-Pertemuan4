4da116a917199b475851e26efd95c780010c93ae
OOP C <arthawebid@gmail.com>
2025-04-12 12:38:18 +0800

    Update Project

---
+++ Array1Dimensi/src/array1dimensi/Array1Dimensi.java
@@ -0,0 +1,17 @@
+package array1dimensi;
+
+/**
+ *
+ * @author Made Artha
+ * TGL: 12 April 2025
+ */
+public class Array1Dimensi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
